e92aaec7adbc6f39f57c74d657cd7337a1cb9041
Anthony Gold <anthgold@gmail.com>
2016-04-29 16:24:58 -0700

    adds to Word.java to add an ID to each word as created; passes

--- src/main/java/Word.java
+++ src/main/java/Word.java
@@ -4,10 +4,12 @@ public class Word {
   private String mTerm;
   // allows us to retrieve all words entered:
   private static ArrayList<Word> instances = new ArrayList<Word>();
+  private int mId; // adds an ID to each word as we create it
 
   public Word(String term) {
     mTerm = term;
     instances.add(this);  // allows us to retrieve all words entered
+    mId = instances.size(); // adds an ID to each word as we create it
   }
 
   public String getTerm() {
@@ -17,6 +19,14 @@ public class Word {
   // allows us to retrieve all words entered:
   public static ArrayList<Word> all() {
     return instances;
+  }
+
+  public static void clear() {
+    instances.clear();
+  }
+
+  public int getId() { // adds an ID to each word as we create it
+    return mId;
   }
 
 }
