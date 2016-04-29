af8211215ef42e48d03f5d82a7dbe9e5f2431597
Anthony Gold <anthgold@gmail.com>
2016-04-29 10:17:34 -0700

    first passing integration test

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -20,7 +20,7 @@ public class AppTest extends FluentTest {
   @Test
   public void rootTest() {
     goTo("http://localhost:4567/");
-    assertThat(pageSource()).contains("Something red; then something green");
+    assertThat(pageSource()).contains("Dictionary");
   }
 
 /*  @Test  // nameOfFunction_testCase_expectedResult()
