a68b936411c9eb89dbfa76f2a6e70414c87de2cf
Anthony Gold <anthgold@gmail.com>
2016-05-04 07:59:31 -0700

    pass method all()

--- src/main/java/Word.java
+++ src/main/java/Word.java
@@ -3,14 +3,13 @@ import java.util.ArrayList;
 public class Word {
 
   private String mTerm;
-  // allows us to retrieve all words entered:
   private static ArrayList<Word> instances = new ArrayList<Word>();
-  private int mId; // adds an ID to each word as we create it
+  private int mId;
 
   public Word(String term) {
     mTerm = term;
-    instances.add(this);  // allows us to retrieve all words entered
-    mId = instances.size(); // adds an ID to each word as we create it
+    instances.add(this);
+    mId = instances.size();
   }
 
   public String getTerm() {
@@ -20,5 +19,11 @@ public class Word {
   public static void clear() {
     instances.clear();
   }
+
+  public static ArrayList<Word> all() {
+    return instances;
+  }
+
+
 
 }
