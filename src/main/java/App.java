3a14254c8bf8e192f16ccc68c88c12897023a87f
Anthony Gold <anthgold@gmail.com>
2016-05-04 19:38:35 -0700

    test root route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,42 +1,41 @@
-import java.util.ArrayList; // do I no longer need this?
-import java.util.Map;
 import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
-  public static void main(String[] args) {
-    staticFileLocation("/public");
-    String layout = "templates/layout.vtl";
 
-    get("/", (request, response) -> {
-      HashMap<String, Object> model = new HashMap<String, Object>();
-      model.put("template", "templates/index.vtl");
-      return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
-
-    get("definitions/new", (request, response) -> {
-      HashMap<String, Object> model = new HashMap<String, Object>();
-      model.put("template", "templates/definition-form.vtl");
-      return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
-
-    get("/definitions", (request, response) -> {
-      HashMap<String, Object> model = new HashMap<String, Object>();
-      model.put("definitions", Definition.all());
-      model.put("template", "templates/definitions.vtl");
-      return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
-
-    post("/definitions", (request, response) -> {
-      HashMap<String, Object> model = new HashMap<String, Object>();
-      String meaning = request.queryParams("meaning");
-      Definition newDefinition = new Definition(meaning);
-      model.put("template", "templates/success.vtl");
-      return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
-
-  }
+  // public static void main(String[] args) {
+  //   staticFileLocation("/public");
+  //   String layout = "templates/layout.vtl";
+  //
+  //   get("/", (request, response) -> {
+  //     HashMap<String, Object> model = new HashMap<String, Object>();
+  //     model.put("template", "templates/index.vtl");
+  //     return new ModelAndView(model, layout);
+  //   }, new VelocityTemplateEngine());
+  //
+  //   get("definitions/new", (request, response) -> {
+  //     HashMap<String, Object> model = new HashMap<String, Object>();
+      // model.put("template", "templates/definition-form.vtl");
+  //     return new ModelAndView(model, layout);
+  //   }, new VelocityTemplateEngine());
+  //
+  //   get("/definitions", (request, response) -> {
+  //     HashMap<String, Object> model = new HashMap<String, Object>();
+  //     model.put("definitions", Definition.all());
+  //     model.put("template", "templates/definitions.vtl");
+  //     return new ModelAndView(model, layout);
+  //   }, new VelocityTemplateEngine());
+  //
+  //   post("/definitions", (request, response) -> {
+  //     HashMap<String, Object> model = new HashMap<String, Object>();
+  //     String meaning = request.queryParams("meaning");
+  //     Definition newDefinition = new Definition(meaning);
+  //     model.put("template", "templates/success.vtl");
+  //     return new ModelAndView(model, layout);
+  //   }, new VelocityTemplateEngine());
+  //
+  // }
 
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -3,11 +3,11 @@ import org.junit.ClassRule;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.fluentlenium.core.filter.FilterConstructor.*;
 
 public class AppTest extends FluentTest {
+
   public WebDriver webDriver = new HtmlUnitDriver();
 
   @Override
@@ -22,37 +22,45 @@ public class AppTest extends FluentTest {
   public void rootTest() {
     goTo("http://localhost:4567/");
     assertThat(pageSource()).contains("Dictionary");
+    assertThat(pageSource()).contains("View Word List");
+    assertThat(pageSource()).contains("Add a New Word");  
   }
 
-  @Test
-  public void definitionIsCreatedTest() {
-    goTo("http://localhost:4567/");
-    click("a", withText("Add a new definition"));
-    fill("#meaning").with("contrition");
-    submit(".btn");
-    assertThat(pageSource()).contains("Your definition has been added.");
-  }
-
-  @Test
-  public void definitionIsDisplayedTest() {
-    goTo("http://localhost:4567/definitions/new");
-    fill("#meaning").with("contrition");
-    submit(".btn");
-    click("a", withText("View definitions"));
-    assertThat(pageSource()).contains("contrition");
-  }
-
-  @Test
-  public void multipleDefinitionsAreDisplayedTest() {
-    goTo("http://localhost:4567/definitions/new");
-    fill("#meaning").with("contrition");
-    submit(".btn");
-    goTo("http://localhost:4567/definitions/new");
-    fill("#meaning").with("home");
-    submit(".btn");
-    click("a", withText("View definitions"));
-    assertThat(pageSource()).contains("contrition");
-    assertThat(pageSource()).contains("home");
-  }
+  // @Test
+  // public void rootTest() {
+  //   goTo("http://localhost:4567/");
+  //   assertThat(pageSource()).contains("Dictionary");
+  // }
+  //
+  // @Test
+  // public void definitionIsCreatedTest() {
+  //   goTo("http://localhost:4567/");
+  //   click("a", withText("Add a new definition"));
+  //   fill("#meaning").with("contrition");
+  //   submit(".btn");
+  //   assertThat(pageSource()).contains("Your definition has been added.");
+  // }
+  //
+  // @Test
+  // public void definitionIsDisplayedTest() {
+  //   goTo("http://localhost:4567/definitions/new");
+  //   fill("#meaning").with("contrition");
+  //   submit(".btn");
+  //   click("a", withText("View definitions"));
+  //   assertThat(pageSource()).contains("contrition");
+  // }
+  //
+  // @Test
+  // public void multipleDefinitionsAreDisplayedTest() {
+  //   goTo("http://localhost:4567/definitions/new");
+  //   fill("#meaning").with("contrition");
+  //   submit(".btn");
+  //   goTo("http://localhost:4567/definitions/new");
+  //   fill("#meaning").with("home");
+  //   submit(".btn");
+  //   click("a", withText("View definitions"));
+  //   assertThat(pageSource()).contains("contrition");
+  //   assertThat(pageSource()).contains("home");
+  // }
 
 }
